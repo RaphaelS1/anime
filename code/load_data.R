295c2a4fa3e3c5ecc64d4c7d96fab8bd992d3d6b
RaphaelS1 <raphaelsonabend@gmail.com>
2021-12-03 16:41:24 +0000

    data loading

--- code/load_data.R
+++ code/load_data.R
@@ -1,2 +1,25 @@
 # Load scraped data
-df <- jsonlite::read_json("data/data.txt")
+anime_list <- jsonlite::read_json("data/data.txt")
+anime_list <- lapply(anime_list, function(x) {
+  x$title <- NULL
+  x$startDate <- as.Date(
+    sprintf("%s-%s-01", x$startDate$year, x$startDate$month)
+  )
+  x$endDate <- as.Date(
+    sprintf("%s-%s-01", x$endDate$year, x$endDate$month)
+  )
+  if (length(x$endDate) == 0) {
+    x$endDate <- NA
+  }
+  x$genres <- paste0(
+    x$genres,
+    collapse = ","
+  )
+  x$tags <- paste0(
+    vapply(x$tags, "[[", character(1), "name")[seq(min(3, length(x$tags)))],
+    collapse = ","
+  )
+  x
+})
+
+anime_df <- do.call(rbind, anime_list)
